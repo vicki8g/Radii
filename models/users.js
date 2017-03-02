416716a49c80d316d6647f0b60d6a189221f1e2f
Ali Salman <Ali Salman>
2017-03-02 13:20:39 -0600

    get user data for profile view

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Giving the Author model a name of type STRING
     name: DataTypes.STRING,
-    // userId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
     email: DataTypes.STRING,
     user_pic: DataTypes.STRING
   });
