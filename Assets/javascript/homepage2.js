4298ce4410f6e37f835547e79b2a2b2961f2bbc6
niquewill <niquewill44@gmail.com>
2017-03-03 21:39:18 -0600

    changed slider 7 times dropped the modal so many changes

---
+++ Assets/javascript/homepage2.js
@@ -0,0 +1,14 @@
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+
+$('#"#myCarousel")').carousel();
