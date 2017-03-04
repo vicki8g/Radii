eaf46b158c237e3c0a5da15f904d0c7abfaf4e84
Ali Salman <Ali Salman>
2017-03-04 07:58:34 -0600

    pushing semi-final project

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-    var url = window.location.search;
-    var userId = url.split("=")[1];
-    getUserData(userId);
-    function getUserData(user) {
-        userId = "/?user_id=" + userId;
-        $.get("/api/users" + userId, function(data) {
-            buildTheProfile(data);
-        });
-    };
-    function buildTheProfile(userData) {
-        var oldSrc = 'http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg';
-        var newSrc = userData.user_pic;
-        $('img[src="' + oldSrc + '"]').attr('src', newSrc);
-    }
-});
+// $(document).ready(function() {
+//     var url = window.location.search;
+//     var userId = url.split("=")[1];
+//     getUserData(userId);
+//     function getUserData(user) {
+//         userId = "/?user_id=" + userId;
+//         $.get("/api/users" + userId, function(data) {
+//             buildTheProfile(data);
+//         });
+//     };
+//     function buildTheProfile(userData) {
+//         var oldSrc = 'http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg';
+//         var newSrc = userData.user_pic;
+//         $('img[src="' + oldSrc + '"]').attr('src', newSrc);
+//     }
+// });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,4 +19,8 @@ module.exports = function(app) {
     app.get("/profile", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/profile.html"));
     });
+
+    app.get("/addgroup", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/groupadd.html"));
+    });
 };
